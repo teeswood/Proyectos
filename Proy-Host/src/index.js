1d81d5c4aa769f00cfff16749b523777cdd9a255
Noteboob <46681311+teeswood@users.noreply.github.com>
2019-03-23 16:54:32 -0300

    Pagina de Vente de Hosting
    Programacion  de Backends

---
+++ Proy-Host/src/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+//initializations
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsdir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '_hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs')
+
+//Middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//Global Variables
+app.use((req, res, next) => {
+
+    next()
+});
+
+//Routes
+app.use(require('./routes'));
+
+//Public
+
+//Starting the server
+app.listen(app.get('port'), () => {
+    console.log('server on port', app.get('port'))
+});
